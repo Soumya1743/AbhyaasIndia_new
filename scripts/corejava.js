816693aa1f5108296358f57e8e30322b813e8a16
Soumya1743 <soumyarangaihagari043@gmail.com>
2024-11-28 00:31:28 +0530

    Updated on Nov28th

---
+++ scripts/corejava.js
@@ -0,0 +1,26 @@
+// let dark = document.getElementById('dark');
+// let light = document.getElementById('light');
+// let container = document.getElementById('container');
+// light.style.display = "none";
+// let jfs_first = document.getElementById('jfs_first');
+// let jfs_second = document.getElementById('jfs_second');
+// let button_syllabus = document.getElementById('button_syllabus');
+
+// function dark_fun() {
+//     container.style.backgroundColor = "#0B192C";
+//     light.style.display = "block";
+//     dark.style.display = "none";
+//     jfs_first.style.backgroundColor = "#1b2c44";
+//     jfs_second.style.backgroundColor = "#1b2c44";
+//     button_syllabus.style.backgroundColor = "#0B192C";
+//     button_syllabus.style.color = "white";
+// }
+// function light_fun() {
+//     container.style.backgroundColor = "white";
+//     dark.style.display = "block";
+//     light.style.display = "none";
+//     jfs_first.style.backgroundColor = "white";
+//     jfs_second.style.backgroundColor = "white";
+//     button_syllabus.style.backgroundColor = "white";
+//     button_syllabus.style.color = "#0B192C";
+// }
